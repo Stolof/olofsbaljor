df31a89455627a96dc59b6d6dea4c1db2021ff33
Stolof <olof.marklund@valtech.com>
2020-09-12 12:53:17 +0200

    Recipe dummy added

---
+++ olofsbaljor/components/recipe.js
@@ -0,0 +1,27 @@
+
+
+export default function Recipe(){
+
+    return (
+        <div>
+            <h1> Recipe titel</h1>
+            <h2> Recipe titel</h2>
+            <img></img>
+            <div>
+                <h3>Ingredients</h3>
+                <ul>
+                    <li> One</li>
+                    <li> One</li>
+                    <li> One</li>
+                </ul>
+            </div>
+            <div>
+
+            <h3> How to</h3>
+            <p>
+                Lorem ipsum
+            </p>
+            </div>
+        </div>
+    )
+}
