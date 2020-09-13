b5b229d4206ffccaf6d59396bada1196e476ea44
Stolof <olof.marklund@valtech.com>
2020-09-13 09:49:48 +0200

    Recipe now loaded with rich text

--- olofsbaljor/components/recipe.js
+++ olofsbaljor/components/recipe.js
@@ -1,18 +1,16 @@
-
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 export default function Recipe({recipe}){
     const title = recipe.title
-    const description = recipe.description.content[0].content[0].value
-    const ingredients = recipe.ingredients.content[0].content // [0] // .content[0].content[0].value
-    const ingredientsItem = ingredients[0].content[0].content[0].value
 
-    //console.log('RECIPE: ', recipe);
-    //console.log('Titel: ', recipe.title);
-    //console.log('description: ', recipe.description.content[0].content[0].value);
-    console.log('ingredients: ', ingredients);
-    console.log('ingredients: ', ingredientsItem);
-    //console.log('instructions: ', recipe.instructions);
+    const description = documentToReactComponents(recipe.descriptionRt) 
+    const ingredients = documentToReactComponents(recipe.ingredientsRt)
+    const instructions = documentToReactComponents(recipe.instructionsRt)     
     
+    /*console.log('Titel: ', title);
+    console.log('Description: ', description);
+    console.log('ingredients: ', ingredients);
+    console.log('instructions: ', instructions);*/
 
     return (
         <div>
@@ -20,22 +18,12 @@ export default function Recipe({recipe}){
             <h2>{description}</h2>
             <div>
                 <h3>Ingredients</h3>
-                <ul>
-                {recipe.ingredients.content.forEach(element => {
-                  <li> {element}</li>  
-                })}
-                </ul>
-                <ul>
-                    <li> One</li>
-                    <li> One</li>
-                    <li> One</li>
-                </ul>
+                {ingredients}
             </div>
             <div>
-
             <h3> How to</h3>
             <p>
-                Lorem ipsum
+                {instructions}
             </p>
             </div>
         </div>
